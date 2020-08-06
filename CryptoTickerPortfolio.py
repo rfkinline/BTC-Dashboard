4f94e744534d5e42065da741b94f074bfd45dc86
rfkinline <robert@lightdec.com>
2020-08-06 14:53:33 -0400

    improvent of GUI

--- CryptoTickerPortfolio.py
+++ CryptoTickerPortfolio.py
@@ -3,9 +3,7 @@
 
 import pandas as pd
 import requests
-import json
 import time
-import argparse
 import sys
 import datetime
 import csv
@@ -20,44 +18,37 @@ pricebtc = 0
 base = 0
 summe = 0
 now = datetime.datetime.now()
-basemaxtime = datetime.datetime.now()
 
-df = pd.read_csv('portfolio.csv', delimiter=';', names = ['Coin', 'Symbol', 'Qty'])
+df = pd.read_csv('portfolio.csv', delimiter=';', names = ['Coin', 'Qty'])
 tf = pd.read_csv('configticker.csv', delimiter=';', names = ['Name', 'Value', 'Zeit'])
 for i in range(len(tf)) :
 	if tf.loc[i,"Name"] == "summemax":
 		summemax = float(tf.loc[i,"Value"])
-
+		summemaxtime = str(tf.loc[i,"Zeit"])
 	if tf.loc[i,"Name"] == "basemax":
-		basemax = str(tf.loc[i,"Value"])
+		basemax = float(tf.loc[i,"Value"])
 		basemaxtime = str(tf.loc[i,"Zeit"])
 
-# Command line arguments to set display type and colour, and enter your name
-parser = argparse.ArgumentParser()
-parser.add_argument('--type', '-t', type=str, required=True, choices=["what", "phat"], help="type of display")
-parser.add_argument('--colour', '-c', type=str, required=True, choices=["white", "black", "yellow"], help="ePaper display colour")
-parser.add_argument('--target', '-n', type=str, required=False, help="eur or usd")
-args = parser.parse_args()
-colour = args.colour
-
-        # Set up the correct display and scaling factors
-if args.type == "phat":
-	inky_display = InkyPHAT(colour)
+displaytype = "phat"
+# Set up the correct display and scaling factors
+if displaytype == "phat":
+	inky_display = InkyPHAT("yellow")
 	scale_size = 1
 	padding = 0
-elif args.type == "what":
-	inky_display = InkyWHAT(colour)
+elif displaytype == "what":
+	inky_display = InkyWHAT("yellow")
 	scale_size = 2.20
 	padding = 15
+
 # Load the fonts
-fredoka_font = ImageFont.truetype(FredokaOne, int(30 * scale_size))
-hanken_bold_font = ImageFont.truetype(HankenGroteskMedium, int(14 * scale_size))
-hanken_medium_font = ImageFont.truetype(HankenGroteskBold, int(30 * scale_size))
+small_font  = ImageFont.truetype(FredokaOne, int(10 * scale_size))
+medium_font = ImageFont.truetype(HankenGroteskMedium, int(14 * scale_size))
+large_font  = ImageFont.truetype(HankenGroteskBold, int(30 * scale_size))
+
 # inky_display.set_rotation(180)
 inky_display.set_border(inky_display.BLACK)
 
 # Create a new canvas to draw on
-
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
 
@@ -73,44 +64,50 @@ while True:
 			if df.loc[i,"Coin"] == "bitcoin":
 				pricebtc = pricecoin
 	except:
-		print("Error reading Coin API")
+		print("Error reading Coin URL", df.loc[i,"Coin"])
 
-	base = str(round(summe / pricebtc, 2))
-	if (base >= basemax):
-		basemax = base
+	base = float(round(summe / pricebtc, 2))
+
+	if (base > basemax):
+
 		with open('configticker.csv', 'w', newline='') as csvfile:
+			basemaxtime = datetime.datetime.now()
 			savwriter = csv.writer(csvfile, delimiter=';')
 			text2=["basemax"] + [basemax] + [basemaxtime]
 			savwriter.writerow(text2)
-			text2=["summemax"] + [summemax] + [basemaxtime]
+			text2=["summemax"] + [summemax] + [summemaxtime]
 			savwriter.writerow(text2)
-
+		basemax = base
 
 	summe = summe / 1000
 
-	if (summe >= summemax):
-		summemax = summe
+	if (summe > summemax):
+		summemaxtime = datetime.datetime.now()
 		with open('configticker.csv', 'w', newline='') as csvfile:
 			savwriter = csv.writer(csvfile, delimiter=';')
-			text2=["summemax"] + [summemax] + [basemaxtime]
+			text2=["summemax"] + [summemax] + [summemaxtime]
 			savwriter.writerow(text2)
 			text2=["basemax"] + [basemax] + [basemaxtime]
 			savwriter.writerow(text2)
+		summemax = summe
 
 	summemaxprint = str(round(summemax,2))
-	summeprint = str(round(summe,2))
+	summeprint    = str(round(summe,2))
+	basemaxprint  = str(basemax)
+	baseprint     = str(base)
 	print(summeprint)
 	summe = 0
 
 	# Grab the text to be displayed
-	text1 = basemaxtime #.strftime("%Y-%m-%d %H:%M")
-	text2 =  str(base) + " - " + str(basemax)
-	text22 = " "
-	text3 = str(summeprint) + " - " + str(summemaxprint)
+	text1 = str(basemaxtime) #.strftime("%Y-%m-%d %H:%M")
+	text2 =  str(baseprint) + " - " + str(basemaxprint)
+	text3 = " "
+#	text3 = str(summemaxtime)
+	text4 = str(summeprint) + " - " + str(summemaxprint)
 
-	# Top and bottom y-coordinates for the white strip
+	# Top and bottom y-coordinates for rowbackground
 	y_top = int(inky_display.HEIGHT * (5.0 / 10.0))
-	y_bottom = y_top + int(inky_display.HEIGHT * (4.0 / 10.0))
+	y_bottom = y_top + int(inky_display.HEIGHT * (5.0 / 10.0))
 
 	# Draw the red, white, and red strips
 
@@ -122,34 +119,35 @@ while True:
 	  for x in range(0, inky_display.width):
 	    img.putpixel((x, y), inky_display.BLACK)
 
-	for y in range(y_bottom, inky_display.HEIGHT):
-	  for x in range(0, inky_display.width):
-	    img.putpixel((x, y), inky_display.RED)
+#	for y in range(y_bottom, inky_display.HEIGHT):
+#	  for x in range(0, inky_display.width):
+#	    img.putpixel((x, y), inky_display.RED)
 
-	# Calculate the positioning and draw the 1st text
-
-	row1_w, row1_h = hanken_bold_font.getsize(text1)
+	# Calculate the positioning and draw the 1st row
+	row1_w, row1_h = medium_font.getsize(text1)
 	row1_x = int((inky_display.WIDTH - row1_w) / 2)
 	row1_y = 0 + padding
-	draw.text((row1_x, row1_y), text1, inky_display.BLACK, font=hanken_bold_font)
-	# Calculate the positioning and draw the 2nd texts
+	draw.text((row1_x, row1_y), text1, inky_display.BLACK, font=medium_font)
 
-	text2_w, text2_h = hanken_medium_font.getsize(text2)
-	text2_x = int((inky_display.WIDTH - text2_w) / 2)
-	text2_y = row1_h + (2 * padding)
-	draw.text((text2_x, text2_y), text2, inky_display.WHITE, font=fredoka_font)
+	# Calculate the positioning and draw the 2nd row
+	row2_w, row2_h = large_font.getsize(text2)
+	row2_x = int((inky_display.WIDTH - row2_w) / 2)
+	row2_y = row1_h + (2 * padding)
+	draw.text((row2_x, row2_y), text2, inky_display.WHITE, font=large_font)
 
-	text22_w, text22_h = hanken_medium_font.getsize(text22)
-	text22_x = int((inky_display.WIDTH - text22_w) / 2)
-	text22_y = text2_y + text2_h - padding
-	draw.text((text22_x, text22_y), text22, inky_display.WHITE, font=fredoka_font)
 
-	# Calculate the positioning and draw the 3rd text
+	# Calculate the positioning and draw the 3rd row
+	row3_w, row3_h = small_font.getsize(text3)
+	row3_x = int((inky_display.WIDTH - row3_w) / 2)
+	row3_y = row2_y + row2_h - padding
+	draw.text((row3_x, row3_y), text3, inky_display.WHITE, font=small_font)
 
-	name_w, name_h = fredoka_font.getsize(text3)
-	name_x = int((inky_display.WIDTH - name_w) / 2)
-	name_y = int(y_top + ((y_bottom - y_top - name_h) / 2))
-	draw.text((name_x, name_y), text3, inky_display.WHITE, font=fredoka_font)
+	# Calculate the positioning and draw the 4th row
+	row4_w, row4_h = large_font.getsize(text4)
+	row4_x = int((inky_display.WIDTH - row4_w) / 2)
+	row4_y = int(y_top + ((y_bottom - y_top - row4_h) / 2))
+	draw.text((row4_x, row4_y), text4, inky_display.WHITE, font=large_font)
+
 
 	# Display the completed name badge
 
--- README.md
+++ README.md
@@ -2,11 +2,16 @@ Instructions for cointickerportfolio.py
 
 
 What I am currently working on:
-display best performing coin (24hrs)
-display 
+1. display: best performing coin (24hrs)
+2. use larger display (3.5")
+3. error: when 404 on url, then the values are updated incorrectly (16.18 - 8.53)
+4. It looks like the display stops working after a few hours
+5. Improve GUI
 
 # install automatic start of program after reboot
-@reboot python3 /home/pi/CryptoTickerPortfolio.py --type phat --colour yellow --target USD &
+crontab -e
+and insert the following code at the end:
+@reboot python3 /home/pi/CryptoTickerPortfolio.py &
 
 # install inky for the display
 https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-inky-phat
@@ -15,11 +20,13 @@ https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-inky-phat
 # for read-write the csv files install Pandas
 sudo apt-get install python3-pandas
 
-Create a file called cointicker.csv with the following content
+Create a file called cointicker.csv with the following content. Only when starting for the first time. After that the program will maintain these values
 summemax;0.1;2011-08-05 10:58
 basemax;0.01;2011-08-05 10:58
 
-summemx = the current value of your portfolio in USD
+summemax = the current value of your portfolio in USD
 basemax = the current value of your portfolio in BTC
 
 Create a file called portfolio.csv that includes your portfolio
+Column coin: this will be used to complete the CoinGecko URL: https://api.coingecko.com/api/v3/coins/ampleforth
+Column Qty: quantity in your portfolio
